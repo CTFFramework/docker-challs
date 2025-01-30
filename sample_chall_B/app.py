f338316741cbbe5853a077f6d47f1b26ac928f49
CTFFramework <ctfframeworkmp@gmail.com>
2025-01-30 13:52:29 +0800

    Sample Challenge for User Testing

---
+++ sample_chall_B/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Access environment variable and display it
+    my_env_var = os.getenv('FLAG_VALUE', 'Environment variable not set')
+    return f"Challenge B's FLAG_VALUE: {my_env_var}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
